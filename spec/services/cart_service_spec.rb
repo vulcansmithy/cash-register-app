2ebb1691edd5d2a7ea6955d551f0e331b3c670a9
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 04:33:46 +0800

    WIP: setup a 'applies BOGO discount for Green Tea' test. rspec test currently failing.

--- spec/services/cart_service_spec.rb
+++ spec/services/cart_service_spec.rb
@@ -8,8 +8,12 @@ RSpec.describe CartService do
   let(:coffee) { FactoryBot.create(:product, code: 'CF1', price: 11.23) }
   
   describe '#calculate_total_with_discounts' do
-    it 'intentionally fail' do
-      expect(true).to eq(false)
-    end
+    it 'applies BOGO discount for Green Tea' do
+          service = CartService.new
+          service.add_item('GR1')
+          service.add_item('GR1')
+
+          expect(service.calculate_total_with_discounts).to eq(3.11)
+        end
   end
 end
