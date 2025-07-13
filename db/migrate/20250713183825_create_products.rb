ad2462c2d69b8afdbe81f5b7f43185e1438e21f6
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 02:42:56 +0800

    WIP: define the attributes for the Product model.

--- db/migrate/20250713183825_create_products.rb
+++ db/migrate/20250713183825_create_products.rb
@@ -1,4 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[7.2]
   def change
+    create_table :products do |t|
+      t.string :code
+      t.string :name
+      t.decimal :price
+
+      t.timestamps
+    end
   end
 end
