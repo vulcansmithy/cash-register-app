5bc4a9d5f626c972eb84fe758a93ef998e95a4bf
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 03:47:49 +0800

    WIP: flesh out the attributes for CartItem model.

--- db/migrate/20250713193857_create_cart_items.rb
+++ db/migrate/20250713193857_create_cart_items.rb
@@ -1,4 +1,11 @@
 class CreateCartItems < ActiveRecord::Migration[7.2]
   def change
+    create_table :cart_items do |t|
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity
+      t.decimal :subtotal
+
+      t.timestamps
+    end
   end
 end
