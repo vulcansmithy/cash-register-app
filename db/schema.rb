2a7ad405b5da33f0c49a5e8aa0ebee345d7db711
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 03:51:50 +0800

    WIP: running migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_13_183825) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_13_193857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.decimal "subtotal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "code", null: false
@@ -21,4 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_13_183825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "cart_items", "products"
 end
