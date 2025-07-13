01a4fa1d77a7e1b7db35f52a2fc3787462e93e6a
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 03:31:09 +0800

    WIP: fleshing out the Prodcut model. Adding validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :code, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
