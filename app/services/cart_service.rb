0c77bddf33f75cadc13a5413aa0002ce2689c0e6
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 04:24:19 +0800

    WIP: create a stub for CartService.

---
+++ app/services/cart_service.rb
@@ -0,0 +1,3 @@
+class CartService
+  #@TODO
+end
--- spec/cart_service_spec.rb
+++ spec/cart_service_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe CartService do
   #@TODO
+  describe '#calculate_total_with_discounts' do
+    it 'intentionally fail' do
+      expect(true).to eq(false)
+    end
+  end
 end
