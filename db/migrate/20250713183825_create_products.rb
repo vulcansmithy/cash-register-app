b18c7700881274985bb66c5989b7b0a06a074bc1
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 02:44:16 +0800

    WIP: quick code refactor for migration file.

--- db/migrate/20250713183825_create_products.rb
+++ db/migrate/20250713183825_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[7.2]
   def change
     create_table :products do |t|
-      t.string :code
-      t.string :name
-      t.decimal :price
+      t.string :code, null: false
+      t.string :name, null: false
+      t.decimal :price, precision: 10, scale: 2, null: false
 
       t.timestamps
     end
