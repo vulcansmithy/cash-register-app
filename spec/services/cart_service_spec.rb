3608a012162443153b88c4bc90a6401df621ba9b
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 04:31:44 +0800

    WIP: setup :green_tea, :strawbery and :coffee test product.

--- spec/services/cart_service_spec.rb
+++ spec/services/cart_service_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe CartService do
   # @TODO
+  
+  let(:green_tea) { FactoryBot.create(:product, code: 'GR1', price: 3.11) }
+  let(:strawberry) { FactoryBot.create(:product, code: 'SR1', price: 5.00) }
+  let(:coffee) { FactoryBot.create(:product, code: 'CF1', price: 11.23) }
+  
   describe '#calculate_total_with_discounts' do
     it 'intentionally fail' do
       expect(true).to eq(false)
