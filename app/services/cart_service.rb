abcd80e890e50b5edf89ac23aef0e459c34d82bc
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-14 04:25:28 +0800

    minor code cleanup.

--- app/services/cart_service.rb
+++ app/services/cart_service.rb
@@ -1,3 +1,3 @@
 class CartService
-  #@TODO
+  # @TODO
 end
--- spec/cart_service_spec.rb
+++ spec/cart_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CartService do
-  #@TODO
+  # @TODO
   describe '#calculate_total_with_discounts' do
     it 'intentionally fail' do
       expect(true).to eq(false)
