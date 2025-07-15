a48399d0747fdf151618748012d6020232d1c4dd
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-15 16:30:43 +0800

    Flesh out the controller.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,10 +1,21 @@
 class CheckoutController < ApplicationController
   def index
+    @products = Product.all
+    @cart_items = CartItem.all
+    @subtotal = CartService.new.calculate_subtotal
+    @total = CartService.new.calculate_total_with_discounts
   end
 
   def add
+    CartService.new.add_item(params[:product_code])
+    redirect_to root_path, notice: "Item added to cart"
+  rescue ArgumentError => e
+    redirect_to root_path, alert: e.message
   end
 
   def remove
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to root_path, notice: "Item removed from cart"
   end
 end
