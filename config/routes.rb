0fb3a892df7ac31e93d984a54176f3934daf6e02
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-15 17:08:34 +0800

    Fix error on doing delete action method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,13 @@
 Rails.application.routes.draw do
   # Root route to the checkout index
-  root 'checkout#index'
-  
+  root "checkout#index"
+
   # Routes for the checkout controller
-  resources :checkout, only: [:index] do
+  resources :checkout, only: [ :index ] do
     # Add routes for add and remove actions
     collection do
       post :add
       delete :remove
     end
   end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
