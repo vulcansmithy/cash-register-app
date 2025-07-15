b6b7705f3482573853840ae478421ce04c04c029
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2025-07-15 17:51:48 +0800

    Remove uncessary rspec test and retaining what is important.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def humanized_money_with_symbol_with_cents(money_object)
+    number_to_currency(money_object, 
+      precision: 2,
+      strip_insignificant_zeros: false,
+      unit: money_object.currency.symbol)
+  end
 end
--- spec/factories/cart_items.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :cart_item do
-    product { nil }
-    quantity { 1 }
-    subtotal { "9.99" }
-  end
-end
